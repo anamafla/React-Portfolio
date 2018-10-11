f8189448d23b0b4f7870f73f47cf46d680ba8276
Ana Mafla <anamafla@hotmail.com>
2018-10-10 19:44:25 -0500

    Portfolio Website and Landing Page

---
+++ src/components/landingpage.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://cf.ltkcdn.net/socialnetworking/images/std/168796-281x281-girl-swear-icon.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                NodeJS
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin" aria-hidden="true" />
+                </a>
+
+                {/* Github */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-github-square" aria-hidden="true" />
+                </a>
+                {/* Freecodecamp */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-free-code-camp" aria-hidden="true" />
+                </a>
+                {/* Youtube */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-youtube-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Landing;
