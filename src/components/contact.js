d82f082ed9edfc929b91fbfa1cc8bec5676ab2c5
Ana Mafla <anamafla@hotmail.com>
2018-10-12 20:46:07 -0500

    Designing the contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
+import { Grid, Cell, List, ListItemContent, ListItem } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Ana M M</h2>
+            <img
+              src="https://cf.ltkcdn.net/socialnetworking/images/std/168796-281x281-girl-swear-icon.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis porro omnis sint debitis unde suscipit sequi, et
+              laboriosam itaque eligendi, minus tenetur obcaecati? Dolorum modi
+              explicabo molestias aspernatur officiis, quo quas fugiat labore
+              odio similique temporibus.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "25px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (123)456-7890
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "25px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    someone@example.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "25px", fontFamily: "Anton" }}
+                  >
+                    <i className="fab fa-skype" aria-hidden="true" />
+                    MySkypeID
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
